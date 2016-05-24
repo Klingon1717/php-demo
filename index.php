188938e0664c32cb535ae749b085e8666f366790
Whitney Murray <wnksmith92@yahoo.com>
2016-05-24 07:22:42 -0500

    wrote text and added text together

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	//**** php variables start with a string $
+	// $test="I'm a variable";
+	// ****then to display the variable you would
+	// 	echo $test;
+	//**** echo is used to write text in php
+	// echo "This is php!";
+	// ****for double qoutes in a string use escape key
+	// $test="I'm a variable";
+	// $test2="so am I";
+	// ****to concat strings in php its a period instead of +
+	// echo $test.$test2;
+	$number=75;
+		echo $number;
+?>
