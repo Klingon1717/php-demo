59827bfd119b10e3dffe4b1a617e90004b98f62e
Whitney Murray <wnksmith92@yahoo.com>
2016-05-24 07:50:33 -0500

    finished php array intro

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
  <?php
+ 	// ****you can also use unset command to remove variables
+ 	// $name="Spock";
+ 	// 	unset($name);
+ 	// ****removing element from an array
+ 	// 	unset($thirdArray["Germany"]);
+ 	// 		print_r($thirdArray);
+ 	// ****if you want to add another element to an array and you dont know how long it is
+ 	// 	$anotherArray[]="salad";
+ 	// 		print_r($anotherArray);
+ 	// ****if you dont want number position of element to show or keys you can write an array like the example below. Where the country is the key and the language is the value
+ 	// $thirdArray=array(
+ 	// 	"France" => "French",
+ 	// 	"USA" => "English",
+ 	// 	"Germany" => "German"
+ 	// 	);
+ 	// 		print_r($thirdArray);
+ 		// echo "<br /> <br />";
+ 	// ****you can also define a new array by
+ 	// 	$anotherArray[0]="pizza";
+ 	// 	$anotherArray[1]="cheese";
+ 			// print_r($anotherArray);
 // 	****use array command to make an array in php
 	// $myArray=array("pizza", "cake", "candy");
  	// ****use print_r($ + name of your array) and it will display the elements in your array and the position of each element in the array
